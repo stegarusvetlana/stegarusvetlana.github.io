89e50c5371fecfbd4652d5afa15eae71f064c9a9
stegarusvetlana <svetlana@stegaru.ro>
2018-02-18 13:40:30 +0200

    update js

--- work/trb/html/js/script.js
+++ work/trb/html/js/script.js
@@ -1,6 +1,5 @@
 window.onload = function() {
  
-	 // START display/hide div hamburger-menu
     const hamburgerMenu = document.getElementById("hamburger-menu");
     const overlayMobile = document.getElementById("overlay-mobile-menu");
     const headerHidden = document.getElementById("header-hidden");
@@ -11,20 +10,15 @@ window.onload = function() {
     xcloseMenu.addEventListener('click', hideMenu);
     
 
-
-
 	function showHamburgerMenu() {
-		console.log("am intrat");
-		if (overlayMobile.style.display = "none"){
-			(overlayMobile.style.display = "block")
-		}
+		console.log("afisam meniul");
+		overlayMobile.style.display = "block";
+		
 	}
 
 	function hideMenu(){
-		console.log("inchidem");
-		if (overlayMobile.style.display = "block"){
-			(overlayMobile.style.display = "none")
-		}
+		console.log("ascundem");
+		overlayMobile.style.display = "none";
 	}
 }
 
